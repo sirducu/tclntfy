a8747369f7b6cfc2ad64088e84b9389dbd65bd04
Radu <105444213+sirducu@users.noreply.github.com>
2024-02-05 19:14:09 +0200

    Update ntfy.tcl

--- ntfy.tcl
+++ ntfy.tcl
@@ -1,58 +1,34 @@
+#!/usr/bin/tclsh
+package require http
+# Function to handle messages in the channel
+set channel "#idlerpg"
 
-package require Tcl 8.5
-echo "NTFY TCL by DuCu Loaded!"
-namespace eval EmailAlert {
-    variable channel_to_watch ""
-    variable words_to_watch {}
 
-    proc setchannel {irc msg args} {
-        variable channel_to_watch
-        set channel_to_watch [lindex $args 0]
-        $irc reply "Channel set to $channel_to_watch."
-    }
+proc handle_channel_message {nick host handle target text} {
+    global channel
+    # Check if the message is from the correct channel
+    if {[string equal -nocase $target $channel]} {
+        # Log the received message
+        putlog "Received message in $channel from $nick: $text"
 
-    proc setwords {irc msg args} {
-        variable words_to_watch
-        set words_to_watch [split [lindex $args 0]]
-        $irc reply "Words set to [lindex $args 0]."
-    }
+        # Check if the message contains the word "ducu"
+        if {[string match *ducu* $text]  || [string match *lust* $text] || [string match *opTim* $text]} {
+            # Define the URL for the HTTP request
+            set url "http://ntfy.sh/idlerpg3"
 
-    proc perform_command {command} {
-        exec $command
-    }
+            # Define the data to be sent in the HTTP request
+            set data "$text"
 
-    proc doPrivmsg {irc msg} {
-        variable channel_to_watch
-        variable words_to_watch
-
-        set target [lindex $msg 0]
-        set message [lindex $msg 1]
-
-        if {$target eq [$irc nick]} {
-            set words [split $message]
-            if {[llength $words] == 2} {
-                set command [lindex $words 0]
-                set arg [lindex $words 1]
-                if {$command eq "setchannel"} {
-                    setchannel $irc $msg [list $arg]
-                } elseif {$command eq "setwords"} {
-                    setwords $irc $msg [list $arg]
-                }
-            }
-        } elseif {$target eq $channel_to_watch} {
-            foreach word $words_to_watch {
-                if {[string tolower $word] in [string tolower $message]} {
-                    set curl_command "curl -d \"$message\" ntfy.sh/multirpg"
-                    perform_command $curl_command
-                    break
-                }
-            }
+            # Make an HTTP POST request with the data
+            set token [::http::geturl $url -query $data]
+            set response [::http::data $token]
+            ::http::cleanup $token
         }
     }
 }
 
-# Usage:
-# EmailAlert::setchannel ircObject messageArgs channel
-# EmailAlert::setwords ircObject messageArgs "word1 word2 word3 ..."
-# EmailAlert::perform_command "command to execute"
-# EmailAlert::doPrivmsg ircObject messageArgs
+# Bind the handle_channel_message function to handle messages in the channel
+bind pubm - * handle_channel_message
+
+# Set the channel to join
+putlog "LOADED ntfyTCL by DuCu"
